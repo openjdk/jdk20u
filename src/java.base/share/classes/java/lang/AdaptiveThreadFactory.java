e389b455c4280fde536c3a1cc81ed3e324a2367e
LeonardoBohnhoff <leonardobohnhoff@protonmail.com>
2023-04-15 10:47:37 +0000

    test introduction of new Java class

---
+++ src/java.base/share/classes/java/lang/AdaptiveThreadFactory.java
@@ -0,0 +1,18 @@
+package java.lang;
+
+/**
+ * Comment
+ */
+public class AdaptiveThreadFactory {
+
+    /**
+    * Comment
+    */
+    public AdaptiveThreadFactory() {}
+
+    /**
+    * Comment
+    */
+    public static void test() {}
+
+}
